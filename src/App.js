d94dcd70157902bf36458a098b874384535fadf2
sajib-hossan1 <sajib.hossan112@gmail.com>
2022-05-29 17:28:05 +0600

    hotel component design done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home/Home';
+import Hotel from './Components/Hotel/Hotel';
 import List from './Components/List/List';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Route path='/' element={<Home/>} exact/>
           <Route path='/home' element={<Home/>} exact/>
           <Route path='/hotels' element={<List/>}/>
+          <Route path='/hotel' element={<Hotel/>}/>
         </Routes>
       </BrowserRouter>
     </div>
